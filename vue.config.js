f306f30c8b0c63013e48874bad3a18b985874c06
fangqi <hedongxiaoshimei@126.com>
2018-11-18 23:02:27 +0800

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,52 @@
+
+module.exports = {
+  // 部署应用时的基本 URL
+  // baseUrl: process.env.NODE_ENV === 'production' ? '' : '',
+
+  devServer: {
+    // 设置主机地址
+    host: 'localhost',
+    // 设置默认端口
+    port: 8080,
+    // 设置代理
+    proxy: {
+      '/api': {
+        // 目标 API 地址
+        target: 'http://192.168.1.22:8080/',
+        // 如果要代理 websockets
+        ws: true,
+        // 将主机标头的原点更改为目标URL
+        changeOrigin: false,
+      },
+    },
+  },
+
+  // css的处理
+  // css: {
+  //   // 当为true时，css文件名可省略 module 默认为 false
+  //   modules: true,
+  //   // 是否将组件中的 CSS 提取至一个独立的 CSS 文件中,当作为一个库构建时，你也可以将其设置为 false 免得用户自己导入 CSS
+  //   // 默认生产环境下是 true，开发环境下是 false
+  //   extract: false,
+  //   // 是否为 CSS 开启 source map。设置为 true 之后可能会影响构建的性能
+  //   sourceMap: false,
+  //   // 向 CSS 相关的 loader 传递选项(支持 css-loader postcss-loader sass-loader less-loader stylus-loader)
+  //   loaderOptions: {
+  //     css: {},
+  //     less: {},
+  //   },
+  // },
+
+  // 默认在生成的静态资源文件名中包含hash以控制缓存
+  filenameHashing: true,
+
+  // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码 (在生产构建时禁用 eslint-loader)
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+
+  // 如果这个值是一个对象，则会通过 webpack-merge 合并到最终的配置中
+  // 如果你需要基于环境有条件地配置行为，或者想要直接修改配置，那就换成一个函数 (该函数会在环境变量被设置之后懒执行)。该方法的第一个参数会收到已经解析好的配置。在函数内，你可以直接修改配置，或者返回一个将会被合并的对象
+  configureWebpack: {},
+
+  // 对内部的 webpack 配置（比如修改、增加Loader选项）(链式操作)
+  chainWebpack: () => { },
+};
