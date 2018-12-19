1a2b10b2f98045f436aeda61aa69efee697c6b1a
fangqi <hedongxiaoshimei@126.com>
2018-12-19 21:47:04 +0800

    add postItem

---
+++ src/views/Post/PostItem/index.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div class="vtx-postItem">
+    <div class="vtx-postItem__title">{{post.title}}</div>
+    <div class="vtx-postItem__des">{{post.des}}</div>
+    <div class="vtx-postItem__content">{{post.content}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import './style.scss'
+
+@Component<PostItem>({
+  components:{},
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    }
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+})
+
+export default class PostItem extends Vue {
+  created() {}
+  mouted() {}
+  befordestoyed() {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit scss to this component only -->
+<style lang="sass" scoped>
+
+</style>
--- src/views/Post/index.vue
+++ src/views/Post/index.vue
@@ -1,16 +1,21 @@
 
 <template>
   <div class="vtx-post">
-    this is vue template component
+    <div v-for="(post, index) in posts" :key="index">
+      <PostItem :post="post"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import PostItem from '@/views/Post/PostItem/index.vue';
 import './style.scss'
 
 @Component<Post>({
-  components:{},
+  components:{
+    PostItem,
+  },
   props: {},
   computed: {},
   methods: {},
@@ -18,6 +23,65 @@ import './style.scss'
 })
 
 export default class Post extends Vue {
+  posts = [
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+    {
+      title: '标题标题',
+      des: '简介',
+      tag: '分组',
+      content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容',
+      updateTime: '1545225926029'
+    },
+
+  ];
   created() {}
   mouted() {}
   befordestoyed() {}
